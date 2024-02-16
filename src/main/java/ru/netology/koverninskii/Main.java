5b4580b21500811602bd19fdec6059ece09b8179
S.Kover <kovestas1@yandex.ru>
2024-02-16 19:26:49 +0300

    16.02.2024

---
+++ src/main/java/ru/netology/koverninskii/Main.java
@@ -0,0 +1,16 @@
+package ru.netology.koverninskii;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 1003;
+        String b = "Netology number one!";
+        if (a>1000){
+            System.out.println(b);
+        }
+        else {
+            System.out.println("Hello world");
+        }
+
+    }
+}
